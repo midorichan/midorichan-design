345a9e4aa77ee833be49a452cab20a059432ead9
Paula Alonso Ishihara <midorichan@users.noreply.github.com>
2018-10-12 10:59:48 -0300

    Gatsby files

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/paula/Documents/GitHub/midorichan-design/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/paula/Documents/GitHub/midorichan-design/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
