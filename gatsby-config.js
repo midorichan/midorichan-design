3f0a0b79ded8a5be9c53141fccd962f609c9c383
Paula Alonso Ishihara <midorichan@users.noreply.github.com>
2018-10-15 00:54:34 -0300

    Gatsby update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Paula Alonso Ishihara | Experience Designer',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -13,7 +13,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/faviconb-32x32.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
