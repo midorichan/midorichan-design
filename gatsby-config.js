799700e4bf63abf523732bf7e6a93fa74dabe674
Paula Alonso Ishihara <midorichan@users.noreply.github.com>
2018-10-16 17:49:18 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#C70039',
+        theme_color: '#C70039',
         display: 'minimal-ui',
         icon: 'src/images/faviconb-32x32.png', // This path is relative to the root of the site.
       },
