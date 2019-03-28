64d9b1b789c2f0f89dc98b0b3472704604655794
Paula Alonso Ishihara <midorichan@users.noreply.github.com>
2019-03-28 11:45:58 -0300

    Layout updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,15 +57,24 @@ const IndexPage = () => (
     <div className="Content">
       <p>Shores of the cosmic ocean as a patch of light consciousness citizens of distant epochs kindling the energy hidden in matter paroxysm of global death? Network of wormholes star stuff harvesting star light courage of our questions inconspicuous motes of rock and gas permanence of the stars star stuff harvesting star light. Vanquish the impossible not a sunrise but a galaxyrise bits of moving fluff descended from astronomers not a sunrise but a galaxyrise not a sunrise but a galaxyrise.</p>
       <p>Made in the interiors of collapsing stars circumnavigated <Link to="/">Drake Equation</Link> cosmos worldlets explorations. Bits of moving fluff hundreds of thousands vastness is bearable only through love not a sunrise but a galaxyrise hydrogen atoms hundreds of thousands? Bits of moving fluff gathered by gravity great turbulent clouds bits of moving fluff muse about great turbulent clouds. Hundreds of thousands a very small stage in a vast cosmic arena vastness is bearable only through love dream of the mind's eye descended from astronomers the ash of stellar alchemy and billions upon billions upon billions upon billions upon billions upon billions upon billions.</p>
-      <Button to="/" 
-                text="Contact me" />
+      <Button to="/blog" 
+                text="Read More" />
     </div>
     <svg width="100%" height="137" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 25.9222C277 84.6264 433 65.7704 720 25.9222C934.818 -3.90394 1214.06 -5.23943 1442 8.07019C2079 45.2658 2208 63.5339 2560 25.9223V137H0V25.9222Z" fill="#000000"/>
+          <path d="M0 25.9222C277 84.6264 433 65.7704 720 25.9222C934.818 -3.90394 1214.06 -5.23943 1442 8.07019C2079 45.2658 2208 63.5339 2560 25.9223V137H0V25.9222Z" fill="#eeeeee"/>
      </svg>
-     <div className="Wave-black">Shores of the cosmic ocean as a patch of light consciousness citizens of distant epochs kindling the energy hidden in matter paroxysm of global death? Network of wormholes star stuff harvesting star light courage of our questions inconspicuous motes of rock and gas permanence of the stars star stuff harvesting star light.</div>
+     <div className="Wave">
+        <div className="Headline">
+          <h2>Nice to meet you.</h2>
+          <div className="Underline"></div>
+        </div>
+        <div className="Content">
+        <p>Shores of the cosmic ocean as a patch of light consciousness citizens of distant epochs kindling the energy hidden in matter paroxysm of global death? Network of wormholes star stuff harvesting star light courage of our questions inconspicuous motes of rock and gas permanence of the stars star stuff harvesting star light.</p>
+        <p>Email me at <a href="mailto:someone@site.com">someone@site.com</a> or contact me through the links below.</p>
+        </div>
+     </div>
         <svg width="100%" height="161" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2560 107.25C2402 149.981 2347 161 2197 161C2047 161 1704.44 45.803 1406 45.803C1120 45.803 934 199.065 0 107.25V0L2560 0V107.25Z" fill="#000000" />
+      <path d="M2560 107.25C2402 149.981 2347 161 2197 161C2047 161 1704.44 45.803 1406 45.803C1120 45.803 934 199.065 0 107.25V0L2560 0V107.25Z" fill="#eeeeee" />
     </svg>
   </Layout>
 )
